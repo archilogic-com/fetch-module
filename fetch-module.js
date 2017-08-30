2e23be980bffbfabff22a9863b146f04a1f0ea57
Tomas Polach <tomas@3d.io>
2017-08-30 14:45:02 +0200

    Update fetch-module.js

--- fetch-module.js
+++ fetch-module.js
@@ -13,37 +13,40 @@
   // global module container
   window.___modules = window.___modules || {}
 
-  // return module if it has been loaded already
-  if (window.___modules[url]) {
-    return Promise.resolve(window.___modules[url])
+  function fetchModule (url) {
+    // return module if it has been loaded already
+    if (window.___modules[url]) {
+      return window.___modules[url].promise
 
-  } else {
-  // load code and use module wrapper
-    return fetch(url).then(function(response){
-      return response.text()
-    }).then(function(code){
+    } else {
+    // load code and use module wrapper
+      window.___modules[url] = { module: { exports: {} } }
+      window.___modules[url].promise = fetch(url).then(function(response){
+        return response.text()
+      }).then(function(code){
 
-      var moduleWrapper
-      // check module type (i know this can be improved. help is welcome)
-      if (code.indexOf('define(function()') > -1) {
-        // AMD
-        moduleWrapper = code+'\nfunction define(cb){ window.___modules["'+url+'"] = cb(); };'
-      } else {
-        // CommonJS
-        moduleWrapper = 'window.___modules["'+url+'"] = (function(){ var exports = {}, module = {exports:exports};'+code+'\nreturn module.exports\n})()'
-      }
+        var moduleWrapper
+        // check module type (i know this can be improved. help is welcome)
+        if (code.indexOf('define(function()') > -1) {
+          // AMD
+          moduleWrapper = code+'\nfunction define(cb){ window.___modules["'+url+'"].module.exports = cb(); };'
+        } else {
+          // CommonJS
+          moduleWrapper = '(function(){ var module = window.___modules["'+url+'"].module, exports = module.exports; '+code+'\n})()'
+        }
 
-      var script = document.createElement('script')
-      try {
-        script.appendChild(document.createTextNode(moduleWrapper))
-        document.body.appendChild(script)
-      } catch (e) {
-        script.text = moduleWrapper
-        document.body.appendChild(script)
-      }
-      
-      return window.___modules[url]
-    })
+        var script = document.createElement('script')
+        try {
+          script.appendChild(document.createTextNode(moduleWrapper))
+          document.body.appendChild(script)
+        } catch (e) {
+          script.text = moduleWrapper
+          document.body.appendChild(script)
+        }
+
+        return window.___modules[url]
+      })
+    }
   }
   
   if (module) module.exports = fetchModule
